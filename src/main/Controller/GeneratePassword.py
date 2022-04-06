648d067bfc7ed49020bcdf9bd0fdf89e50b026e4
FabianKlimczyk <84398772+FabianKlimczyk@users.noreply.github.com>
2022-04-06 21:24:33 +0900

    Implement password calculation

--- src/main/Controller/GeneratePassword.py
+++ src/main/Controller/GeneratePassword.py
@@ -1,3 +1,23 @@
-def getPassword(length, inclUpperCase,inclNumbers,inclSpecialChars):
-    #TODO Logic for calculation
-    pass
+import random as rnd
+
+def getPassword(charList,numberOfChars=int) -> str:
+    password = ""
+    for i in range (numberOfChars):
+        index = rnd.randint(0,len(charList)-1)
+        password = password + rnd.choice(charList[index])
+    return password
+
+def createCharList(charList, inclUpperCase=bool, inclNumbers=bool, inclSpecialChars=bool):
+    charList.append(list('abcdefghijklmnopqrstuvwyxz'))
+    if inclUpperCase:
+        charList.append(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if inclNumbers:
+        charList.append(list('0123456789'))
+    if inclSpecialChars:
+        charList.append(list('.,;-_!?$%&()+#'))
+    return charList
+
+def generatePassword(length=int, inclUpperCase=bool,inclNumbers=bool,inclSpecialChars=bool) -> str:
+    charList = []
+    charList = createCharList(charList,inclUpperCase,inclNumbers,inclSpecialChars)
+    return getPassword(charList,length)
