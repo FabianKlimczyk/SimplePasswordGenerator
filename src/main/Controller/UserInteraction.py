8b33b75ef8331e4513c82e24023ebc4089377603
FabianKlimczyk <84398772+FabianKlimczyk@users.noreply.github.com>
2022-05-06 14:14:56 +0900

    minor code reworks

--- src/main/Controller/UserInteraction.py
+++ src/main/Controller/UserInteraction.py
@@ -14,18 +14,23 @@
                                 - entry id always 0 because of autoincrement
                             function userLoop
                                 - implement filter for loading data
+                                - change data loop while-loop
 
 '''
+#external
+import pyinputplus as pyip
+import sqlite3 as sql3
+import time
+import random
 
+#internal
 from src.main.Controller.GeneratePassword import generatePassword as gnrt_pw
 from src.main.Controller.DeEnCoding import encode as encode
 import src.main.Model.Entry as Entry
 import src.main.Database.DBMgt as db
 import src.main.Database.DataMgt as datamgt
 from src.main.Utility.Strings import getString
-import sqlite3 as sql3
-import time
-import random
+
 
 
 
@@ -39,7 +44,7 @@ def createEntry() -> Entry.Entry:
     name = input(getString("inp_name","EN")+"\n").strip()
     login = input(getString("inp_login","EN")+"\n").strip()
     descr = input(getString("inp_descr","EN")+"\n").strip()
-    pwLength = int(input(getString("inp_pw_length","EN")+"\n").strip())
+    pwLength = pyip.inputInt(getString("inp_pw_length","EN")+"\n", min=6)
     inclUpperCase = input(getString("inp_incl_upper_case","EN")+"\n").lower().strip() == "y"
     inclNumbers = input(getString("inp_incl_numb","EN")+"\n").lower().strip() == "y"
     inclSpecialCharacters = input(getString("inp_incl_spec_char","EN")+"\n").lower().strip() == "y"
@@ -52,16 +57,19 @@ def createEntry() -> Entry.Entry:
     return Entry.Entry(0,name,login,descr,enc_pw,shift,time.time(),time.time())
 
 def userLoop(conn: sql3.Connection) -> None:
-    while action := input("What do you want to do? (c)ontinue or (q)uit? Please insert c or q").lower() != "q":
-        inp = input("Do you want to (c)reate, (l)oad, (u)pdate, (d)elete? ").lower()
+    continueLoop = True
+    while continueLoop:
+        inp = input("Do you want to (c)reate, (l)oad, (u)pdate, (d)elete?\n").lower()
 
         if inp == "c":
+            #create new data
             print("Creating a new entry...")
             entry = createEntry()
             db.insertEntry(conn, entry)
             print("The entry {} was created".format(entry.name))
+
         elif inp == "l":
-            # load and show existent entry
+            # load and show data
             print("Loading...")
             showAll = input("Do you want to see all entries? y/n\n").lower()
             if showAll == "y":
@@ -74,7 +82,9 @@ def userLoop(conn: sql3.Connection) -> None:
                     datamgt.getPassword(conn,column,filterValue)
                 else:
                     datamgt.showData(conn, column, filterValue)
+
         elif inp == "u":
+            #data update
             print("Updating...")
             name = input("Which record do you want to change? Please enter the name!: ")
             column = f" last_modified_on = {time.time()}, "+input("Which column do you want to change?: ") + "= ?"
@@ -83,13 +93,19 @@ def userLoop(conn: sql3.Connection) -> None:
                 print("The entry was updated")
             else:
                 print("Update is not possible. Please try again")
+
         elif inp == "d":
+            #data deletion
             print("Deleting...")
             id = input("Which id do you want to delete? ")
             if db.deleteEntryById(conn, id):
                 print("The entry was deleted")
             else:
                 print("Deletion is not possible! Please try again.")
+
         else:
             print("{} is not a valid input! c/l/u/d !!!".format(inp))
-    print("Program quit!")
+
+        continueLoop = input("Do you want to (c)ontinue?\n").lower() == "c"
+
+    print("Program finished!")
